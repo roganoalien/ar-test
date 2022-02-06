061522c40b3498d1975779e8b7d91af13d94941e
Rodrigo García <hola@rodrigogarcia.com.mx>
2022-02-05 18:55:56 -0600

    ✨ First commit
    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ['./index.html'],
+	theme: {
+		extend: {
+			colors: {
+				mBlack: '#162135',
+				mWhite: '#F9F9FA',
+				mGray: '#F1F1F1',
+			},
+		},
+	},
+	plugins: [],
+};
